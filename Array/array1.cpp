d47c83ccc3356f5940ac79139d3bae2e44ee146a
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2021-09-22 10:18:58 +0530

    Create array1.cpp

---
+++ Array/array1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main(){
+   int a; 
+    cout<<"Enter the size ";
+    cin>>a;
+    cout<<"\nEnter the array elements ";
+    for(int i=0;i<n;i++)
+      cin>>a[i];
+   for(int i=0;i<n;i++)
+      cout<<a[i];
+}
