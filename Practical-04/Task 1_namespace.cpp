1f75d8e3510fe8230df59d8c0786840a44026ced
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2021-09-29 10:32:23 +0530

    Update Task 1_namespace.cpp

--- Practical-04/Task 1_namespace.cpp
+++ Practical-04/Task 1_namespace.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+namespace first 
+{
+  void add(int a , int b)
+  {
+    std::cout<<a+b;
+  }
+}
+namespace second 
+{
+  void add(float a, float b)
+  {
+    std::cout<<a+b;
+  }
+}
+int main ()
+{
+  first::add(2,3);
+  second::add(1.1,2.2);
+}
