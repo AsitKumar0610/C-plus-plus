2c04499015fd81f447affea257e43aacd423c4ac
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2021-10-01 19:01:44 +0530

    Create Task 2.cpp

---
+++ Practical-04/Task 2.cpp
@@ -0,0 +1,24 @@
+//Practical-4[task-2]
+#include<iostream>
+using namespace std;
+namespace first
+{
+    int sum(int a,int b)
+    {
+        return a+b;
+    }
+}
+namespace second
+{
+    float sum(float a,float b)
+    {
+        return a+b;
+    }
+}
+int main()
+{
+    cout<<first::sum(1,2)<<endl;
+    cout<<second::sum(1.2,2)<<endl;
+    cout<<second::sum(1,2.1)<<endl;
+    cout<<second::sum(1.1,2.1)<<endl;
+}
