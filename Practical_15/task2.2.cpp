a4f65f4e4f42880615d83500089ead0af0afe1ec
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2022-01-07 11:06:56 +0530

    Update task2.2.cpp

--- Practical_15/task2.2.cpp
+++ Practical_15/task2.2.cpp
@@ -1,22 +1,14 @@
+
 #include<iostream>
 #include<fstream>
 #include<string>
 using namespace std ;
 
-int main(){
-	fstream  readfile;
-	char data;
-	readfile.open("text.txt");
-	while(1)
-	{
-		readfile >>data;
-		
-		if(readfile.eof())
-			break;
-		else
-			cout << data ;
-	}
-        cout << endl;
-	readfile.close();
+int main()
+{
+	fstream myfile;
+	myfile.open("text.txt");
+	myfile<< "Practical 15 task 2.2 writing in a file using fstream";
+	myfile.close();
 	return 0;
 }
