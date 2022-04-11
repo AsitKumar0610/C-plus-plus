ddeab203c9f7c2fc14b6f4423a153eccc7378ad8
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2022-04-11 14:53:10 +0530

    Add files via upload

---
+++ Count.cpp
@@ -0,0 +1,90 @@
+/*#include<iostream>
+#define MAX 100
+using namespace std;
+
+void countSort(int arr[],int size)
+{
+int output[size];
+int count[size];
+int max = arr[0];
+
+for(int i = 1; i < size ;i++)
+{
+if(arr[i] > max)
+max= arr[i];
+}
+
+for(int i = 0 ; i <= size ; i++)
+{
+count[i]=0;
+}
+
+for(int i = 0 ; i < size ; i++)
+{
+count[arr[i]]++ ;
+}
+
+for(int i = 1; i <=max ;i++)
+{
+count[i] += count[i-1];
+}
+
+for(int i = size -1 ; i >= 0 ;i--)
+{
+output[count[arr[i]]-1] = arr[i] ;
+count[arr[i]]--;
+}
+
+for(int i = 0 ; i < size ; i++)
+{
+arr[i] = output[i];
+}
+}
+
+void printArray(int arr[], int n)
+{
+for(int i =0 ; i < n; i++)
+{
+cout << arr[i] <<endl;
+}
+}
+
+int main ()
+{
+int i , n ;
+cout << "Enter the size of array : ";
+cin >> n ;
+int a[n];
+cout << "Enter the elements of the array : ";
+for(i = 0 ; i < n ; i++)
+{
+cin >> a[i];
+}
+countSort(a,n);
+printArray(a,n);
+}*/
+
+#include<iostream>
+using namespace std ;
+int main()
+{
+int n, count =0 ;
+cout << "Enter the size of the array" << endl ;
+cin >> n ;
+int a[n] ;
+cout << "Enter the elements of the array : ";
+for(int i = 0 ; i < n ; i++)
+{
+cin >> a[i] ;
+}
+
+for(int i = 0 ; i < n ; i++){
+	for(int j = i + 1 ; j < n ; j++){
+		if(a[i] > a[j])
+		{
+			count ++ ;
+		}
+	}
+}
+cout << "No. of inversions : " << count ;
+}
